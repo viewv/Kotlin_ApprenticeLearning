b4399b43f5daeec9229a8ad450ee28f4190941dc
Zhang XueNan <zxn@zxnnet.top>
2018-07-20 20:14:53 +0800

    Ch2.MiniEx Finish

---
+++ Ch.2/src/Mini-exercises.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>) {
+    val myAge : Int = 19
+    var averageAge : Double = 30.0
+    averageAge = (myAge + averageAge) /2
+    println(averageAge)
+    val testNumber = 100
+    val even0dd = testNumber
+    println(even0dd)
+    var answer = 0
+    answer += 1
+    answer += 10
+    answer *= 10
+    answer = answer shr 3
+    println(answer)
+}
