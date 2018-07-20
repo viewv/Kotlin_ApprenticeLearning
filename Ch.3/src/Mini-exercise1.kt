a32226677cfb8538b5c420cd9472b801e347d42e
Zhang XueNan <zxn@zxnnet.top>
2018-07-20 20:45:03 +0800

    The Init Part Of This Book is too Easy For me 😂

---
+++ Ch.3/src/Mini-exercise1.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    val age1 = 42
+    val age2 = 21
+    val arg1 = (age1+age2)/2
+    println("Average = ${arg1}")
+    val smell = "🎁👳‍♂️🤴🧓💂‍♀️🥨🚉🍤"
+    println(smell)
+}
